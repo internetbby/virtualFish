d1e61ae61832e8582416fdb000f7ec447e8f36f5
Linnea <linnea.alse@elev.ga.ntig.se>
2021-03-29 09:59:10 +0200

    first commit

---
+++ virtualFish/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace virtualFish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
