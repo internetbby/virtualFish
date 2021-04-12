90c9720be46e8dfe0c1b02ec57d3d647feac9346
Linnea <linnea.alse@elev.ga.ntig.se>
2021-04-12 09:27:23 +0200

    small changes

--- virtualFish/Program.cs
+++ virtualFish/Program.cs
@@ -6,7 +6,30 @@ namespace virtualFish
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random generator = new Random();
+            string [] names = {"Guppy", "Beta", "Catfish", "Tilapia", "Crab", "Cod" };
+            int chance = generator.Next(0,101);
+
+            Fish f1 = new Fish(); 
+            f1.Name = f1.GetName(names[0]);
+            f1.odds = f1.setOdds (90); 
+            f1.catchRate = f1.setCatchRate(10);
+
+            int amountOfFish = 0; 
+
+
+            for (int i = 0; i < f1.catchRate; i++){
+                if (chance < f1.odds){
+                    amountOfFish++; 
+                    chance = generator.Next(0,101);
+                }
+            } // make this a method : ) 
+
+            System.Console.WriteLine(amountOfFish);
+            
+
+            System.Console.WriteLine(f1.Name);
+            Console.ReadLine();
         }
     }
 }
---
+++ virtualFish/fish.cs
@@ -0,0 +1,23 @@
+public class Fish{
+ public string Name = "";
+  public string GetName(string name)
+  {
+    Name = name; 
+    return name;
+  }  //this sets the name of the fish type
+
+    public int odds = 0; 
+
+    public int setOdds(int newOdds){
+        odds = newOdds;
+        return newOdds; 
+    }
+
+    
+    public int catchRate = 0; 
+
+    public int setCatchRate(int newCatchRate){
+        catchRate = newCatchRate;
+        return newCatchRate; 
+    }
+}
